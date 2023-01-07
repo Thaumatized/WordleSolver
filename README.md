bfc2a25fe1a57272e05f70daa6c6d162f7587d1d
Thaumatized <veeti.junkkala@gmail.com>
2023-01-07 21:30:59 +0200

    Made the thing function with a dictionary and added a short README.md. Currently, words with multiples of the same letter break the system. Solving this issue is WIP.

---
+++ README.md
@@ -0,0 +1,5 @@
+Word filter, useful for solving wordles, such as https://www.nytimes.com/games/wordle/index.html
+
+
+dictionary.txt is words_alpha.txt from: https://github.com/dwyl/english-words
+it may not be perfect for this purpose, as it contains a lot of abbreviations and such.
--- WordleSolver.py
+++ WordleSolver.py
@@ -1,65 +1,128 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-requiredcharacters = ""
-requiredpositionedcharacters = "     "
+import os
+import random
 
-for attempt in range(6):
-	listedsuggestions = []
-	for i in range(len(alphabet)):
-		listedsuggestions.append([])
-	for i1 in range(len(alphabet)):
-		suggestion = "";
-		for i2 in range(len(alphabet)):
-			for i3 in range(len(alphabet)):
-				for i4 in range(len(alphabet)):
-					for i5 in range(len(alphabet)):
-						suggestion = alphabet[i1] + alphabet[i2] + alphabet[i3] + alphabet[i4] + alphabet[i5]
-						skip = False
-						for i in range(len(suggestion)):
-							if requiredpositionedcharacters[i] != ' ' and suggestion[i] != requiredpositionedcharacters[i]:
-								skip = True
-							break
-						for i in range(len(requiredcharacters)):
-							if requiredcharacters[i] not in suggestion:
-								skip = True
-							break
-							
-						if skip:
-							suggestion = ""
-							
-		if suggestion != "":
-			listedsuggestions[i1].append(suggestion)
-	
-	for i in range(len(listedsuggestions)-1, -1, -1):
-			if listedsuggestions[i] != []:
-				listedsuggestions.pop(i)
-	
-	row = 0
-	while True:
-		listsover = True
-		for i in range(len(listedsuggestions)):
-			if len(listedsuggestions[i]) > row:
-				print(listedsuggestions + "     ")
-				listsover = False
-			else:
-				print("          ")
-		if listsover:
-			break
-		row += 1
+
+characters = "abcdefghijklmnopqrstuvwxyz"
+dictionary = []
 	
 	
+def getdictionary(length):
+	if not os.path.exists("StrippedDictionaries"):
+		os.makedirs("StrippedDictionaries")
 	
-	print("alphabet: " + alphabet)
-	print("required characters: " + requiredcharacters)
-	print("positioned characters: " + requiredpositionedcharacters)
+	path = os.path.join("StrippedDictionaries", str(length) + "_dictionary.txt")
 	
-	print("What word did you try?")
-	answer = input()
-	print("Please input the result as a string of numbers. use 0 for not in word, 1 for in word but wrong place and 2 for correct place")
-	evaluation = input()
-	for i in range(len(answer)):
+	if not os.path.exists(path):
+		newdictionary = open(path, "w")
+		masterdictionary = open("dictionary.txt", "r").read().split("\n")
+		for word in masterdictionary:
+			if len(word) == length:
+				newdictionary.write(word.lower() + "\n")
+				
+	#last one is removed, because the automatic generation always makes the last row be empy.
+	return open(path, "r").read().split("\n")[:-1]
+	
+
+#takes words out of a dictionary based on results from wordle
+def stripdictionary(dictionary, word, evaluation):
+	forbiddenletters = ""
+	includedletters = ""
+	forcedletters = []
+	misplacedletters = []
+	for i in range(len(word)):
 		if evaluation[i] == '0':
-			alphabet = alphabet.replace(answer[i], "")
+			forbiddenletters += word[i]
 		if evaluation[i] == '1':
-			requiredcharacters += answer[i]
+			includedletters += word[i]
+			misplacedletters.append([word[i], i])
 		if evaluation[i] == '2':
-			requiredpositionedcharacters = requiredpositionedcharacters[:i] + answer[i] + requiredpositionedcharacters[i+1:]
+			forcedletters.append([word[i], i])
+	
+	for i in range(len(dictionary)-1, -1, -1):
+		for forbiddenletter in forbiddenletters:
+			if forbiddenletter in dictionary[i]:
+				dictionary.pop(i)
+				break
+	
+	for i in range(len(dictionary)-1, -1, -1):
+		for includedletter in includedletters:
+			if includedletter not in dictionary[i]:
+				dictionary.pop(i)
+				break
+	
+	for i in range(len(dictionary)-1, -1, -1):
+		for forcedletter in forcedletters:
+			if dictionary[i][forcedletter[1]] != forcedletter[0]:
+				dictionary.pop(i)
+				break
+				
+	for i in range(len(dictionary)-1, -1, -1):
+		for misplacedletter in misplacedletters:
+			if dictionary[i][misplacedletter[1]] == misplacedletter[0]:
+				dictionary.pop(i)
+				break
+				
+	return dictionary
+
+
+print("Welcome to wordle solver.")
+length = -1
+while(length == -1):
+	print("Please input the length of the word")
+	lengthinput = input()
+	if lengthinput.isnumeric():
+		length = int(lengthinput)
+dictionary = getdictionary(length)
+
+print("now to get started, you should start with a word that has as many different characters as possible. Here is a few suggestions.")
+
+for i in range(10):
+	suggestion = ""
+	while True:
+		suggestion = dictionary[int(len(dictionary) * random.random())]
+		breakingout = True
+		for char in suggestion:
+			if suggestion.count(char) > 1:
+				breakingout = False
+		if breakingout:
+			break
+	print(suggestion)
+	
+while True:
+	validinput = False
+	wordinput = ""
+	while not validinput:
+		print("What word did you use?")
+		wordinput = input().lower()
+		validinput = len(wordinput) == length
+		
+	validinput = False
+	evaluationinput = ""
+	while not validinput:
+		print("What did we learn? write a string of numbers, with 0 meaning the letter is not in the word, 1 meaning it is but isn't in the correct position and 2 meaning it is fully correct.'")
+		evaluationinput = input()
+		validinput = len(evaluationinput) == length and len(evaluationinput.replace("0", "").replace("1", "").replace("2", "")) == 0
+	
+	if "0" not in evaluationinput and "1" not in evaluationinput:
+		print("Wohoo!")
+		break
+	
+	dictionary = stripdictionary(dictionary, wordinput, evaluationinput)
+	
+	if len(dictionary) == 0:
+		print("Sorry, it seems the word isn't in my dictionary.")
+		break
+	if len(dictionary) == 1:
+		print("It seems that the only possible answer is:")
+		print(dictionary[0])
+	elif len(dictionary) <= 10:
+		print("We are getting close, only " + str(len(dictionary)) + " possibilities left. Here are all the possibilities I know;")
+		for i in range(len(dictionary)):
+			print(dictionary[i])
+	else:
+		print("there are " + str(len(dictionary)) + " possibilities left. Here are some of them:")
+		for i in range(10):
+			print(dictionary[int(len(dictionary) * random.random())])
+	
+	
+	
